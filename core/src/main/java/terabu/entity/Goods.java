fe1efcab3c95dde671bad29966a5dd9fbec99d04
Lynxire <ya.staravoitav@gmail.com>
2024-04-10 23:20:27 +0300

    added entity: goods and Order

---
+++ core/src/main/java/terabu/entity/Goods.java
@@ -0,0 +1,17 @@
+package terabu.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import terabu.entity.status.GoodsType;
+@Data
+@Entity
+@Table(schema = "app", name = "goods")
+public class Goods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long count;
+    private Long price;
+    private GoodsType type;
+}
---
+++ core/src/main/java/terabu/entity/Order.java
@@ -0,0 +1,21 @@
+package terabu.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import terabu.entity.status.OrderStatus;
+
+@Data
+@Entity
+@Table(schema = "app", name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private OrderStatus status;
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "userId")
+    private User user;
+
+
+}
