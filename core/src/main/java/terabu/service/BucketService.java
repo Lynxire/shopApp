019ef0738fb230678c991019392158639fd43a51
Lynxire <ya.staravoitav@gmail.com>
2024-04-28 02:02:43 +0300

    test

--- core/src/main/java/terabu/service/BucketService.java
+++ core/src/main/java/terabu/service/BucketService.java
@@ -3,36 +3,35 @@ package terabu.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import terabu.dto.bucket.BucketResponse;
 import terabu.entity.Bucket;
 import terabu.entity.Goods;
 import terabu.entity.Order;
 import terabu.entity.User;
 import terabu.entity.status.OrderStatus;
+import terabu.mapper.BucketMapper;
 import terabu.repository.BucketRepository;
 import terabu.repository.GoodsRepository;
 import terabu.repository.OrderRepository;
 import terabu.repository.UserRepositorySpringData;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class BucketService {
+    private final BucketMapper mapper;
     private final BucketRepository bucketRepository;
+    private final GoodsRepository goodsRepository;
     private final OrderRepository orderRepository;
     private final UserRepositorySpringData userRepository;
-    private final GoodsRepository goodsRepository;
 
     @Transactional
-    public void addOrderAndGoodsByBucket(Long userId, Long goodsId, Long goodsCount, Long goodsSum) {
-
-        User user1 = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Пользователь не авторизован"));
+    public BucketResponse addOrderAndGoodsByBucket(Long userId, Long goodsId, Long count) {
 
         Goods goods1 = goodsRepository.findById(goodsId).orElseThrow(() -> new RuntimeException("Товара нет в наличии"));
-        List<Goods> goodsList = new ArrayList<>();
-        goodsList.add(goods1);
 
+        User user1 = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Пользователь не авторизован"));
         Order order = orderRepository.findByUserAndStatus(user1, OrderStatus.CREATE).orElseGet
                 (() -> {
                     Order newOrder = new Order();
@@ -41,19 +40,23 @@ public class BucketService {
                     return orderRepository.save(newOrder);
                 });
 
-        List<Order> orderList = new ArrayList<>();
-        orderList.add(order);
 
+        Long sum = goods1.getPrice() * count;
 
         Bucket bucket = new Bucket();
-        bucket.setOrders(orderList);
-        bucket.setGoods(goodsList);
-        bucket.setCount(goodsCount);
-        bucket.setSum(goodsSum*goodsCount);
-
+        bucket.setId(bucket.getId());
+        bucket.setCount(count);
+        bucket.setSum(sum);
+        bucket.setOrders(Collections.singletonList(order));
+        bucket.setGoods(Collections.singletonList(goods1));
 
         bucketRepository.save(bucket);
+        return mapper.toResponse(bucket);
+
 
     }
+
+
+
 
 }
--- core/src/main/java/terabu/service/OrderService.java
+++ core/src/main/java/terabu/service/OrderService.java
@@ -3,10 +3,12 @@ package terabu.service;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import terabu.entity.Order;
 import terabu.entity.User;
 import terabu.entity.status.OrderStatus;
 import terabu.repository.OrderRepository;
+import terabu.repository.UserRepositorySpringData;
 
 import java.util.List;
 
@@ -14,6 +16,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
-
+    private final UserRepositorySpringData userRepository;
 
 }
