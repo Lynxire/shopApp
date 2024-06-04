b311d6da4220e5981044a96fec84c208fa82171c
Lynxire <ya.staravoitav@gmail.com>
2024-06-04 19:07:45 +0300

    added microservice CommentService

--- core/src/main/java/terabu/service/CommentService.java
+++ core/src/main/java/terabu/service/CommentService.java
@@ -1,59 +1,42 @@
 package terabu.service;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import terabu.dto.comment.CommentRequest;
 import terabu.dto.comment.CommentResponse;
-import terabu.dto.data.UserDataDTO;
-import terabu.dto.users.UserDTO;
-import terabu.entity.Comments;
-import terabu.mapper.CommentMapper;
-import terabu.repository.CommentsRepository;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
-    private final CommentsRepository commentsRepository;
-    private final CommentMapper commentMapper;
-    private final UserService userService;
-    private final UserDataService userDataService;
-    public CommentResponse addComment(CommentRequest commentRequest) {
-        UserDTO user = userService.findUserById(commentRequest.getUserId());
-        Comments comments = commentMapper.toEntity(commentRequest);
-        comments.setUserId(user.getId());
-        commentsRepository.save(comments);
-        return commentMapper.toResponse(comments);
+    private final RestTemplate restTemplate;
+
+    public CommentResponse addComment(@RequestBody @Valid CommentRequest commentRequest) {
+        return restTemplate.postForEntity("http://localhost:8082/comments/add",  commentRequest, CommentResponse.class).getBody();
     }
 
-    public void deleteComment(Long commentId) {
-        commentsRepository.findById(commentId).ifPresent(commentsRepository::delete);
+
+    public String deleteComment(@RequestParam @Min(1) @NotNull Long commentId) {
+        return restTemplate.postForEntity("http://localhost:8082/comments/delete?commentId=" + commentId, null,  String.class).getBody();
 
     }
 
-    public List<CommentResponse> getCommentByUser(Long userId) {
-        UserDataDTO dataDTO = userDataService.findDataByUserId(userId);
-        List<Comments> list = commentsRepository.findAllByUserId(userId);
-        List<CommentResponse> responses = new ArrayList<>();
-        list.forEach(comments -> {
-            CommentResponse commentResponse = commentMapper.toResponse(comments);
-            commentResponse.setName(dataDTO.getName());
-            responses.add(commentResponse);
-        });
-
-        return responses;
+    public List<CommentResponse> getMyComments(@RequestParam @Min(1) @NotNull Long userId) {
+        CommentResponse[] responses = restTemplate.getForEntity("http://localhost:8082/comments/myComments?userId=" + userId, CommentResponse[].class).getBody();
+        return Arrays.asList(responses);
     }
 
-    public List<CommentResponse> getAllComments(){
-        List<Comments> commentsList = commentsRepository.findAll();
 
-        return commentsList.stream().map(comments ->{
-            CommentResponse commentResponse = commentMapper.toResponse(comments);
-            UserDataDTO dataDTO = userDataService.findDataByUserId(comments.getUserId());
-            commentResponse.setName(dataDTO.getName());
-            return commentResponse;
-        }).toList();
+    public List<CommentResponse> getAllComments() {
+        CommentResponse[] responses = restTemplate.getForEntity("http://localhost:8082/comments/allComments", CommentResponse[].class).getBody();
+        return Arrays.asList(responses);
     }
 }
