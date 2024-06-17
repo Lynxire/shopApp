c8b1d1b7070dd94fc7067988b062d96dfa0c89a8
Lynxire <ya.staravoitav@gmail.com>
2024-06-17 15:01:20 +0300

    updated url logic and docker-compose.yaml

--- core/src/main/java/terabu/service/CommentService.java
+++ core/src/main/java/terabu/service/CommentService.java
@@ -1,9 +1,11 @@
 package terabu.service;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,24 +21,39 @@ import java.util.List;
 public class CommentService {
     private final RestTemplate restTemplate;
 
+    @Value("${comments.url}")
+    private String url;
+    private String urlAdd;
+    private String urlDeleteById;
+    private String urlByUserId;
+    private String urlAll;
+
+    @PostConstruct
+    public void init() {
+        urlAdd = url + "/add";
+        urlDeleteById = url + "/delete?commentId=";
+        urlByUserId = url + "/myComments?userId=";
+        urlAll = url + "/allComments";
+    }
+
     public CommentResponse addComment(@RequestBody @Valid CommentRequest commentRequest) {
-        return restTemplate.postForEntity("http://localhost:8082/comments/add",  commentRequest, CommentResponse.class).getBody();
+        return restTemplate.postForEntity(urlAdd, commentRequest, CommentResponse.class).getBody();
     }
 
 
     public String deleteComment(@RequestParam @Min(1) @NotNull Long commentId) {
-        return restTemplate.postForEntity("http://localhost:8082/comments/delete?commentId=" + commentId, null,  String.class).getBody();
+        return restTemplate.postForEntity(urlDeleteById + commentId, null, String.class).getBody();
 
     }
 
     public List<CommentResponse> getMyComments(@RequestParam @Min(1) @NotNull Long userId) {
-        CommentResponse[] responses = restTemplate.getForEntity("http://localhost:8082/comments/myComments?userId=" + userId, CommentResponse[].class).getBody();
+        CommentResponse[] responses = restTemplate.getForEntity(urlByUserId + userId, CommentResponse[].class).getBody();
         return Arrays.asList(responses);
     }
 
 
     public List<CommentResponse> getAllComments() {
-        CommentResponse[] responses = restTemplate.getForEntity("http://localhost:8082/comments/allComments", CommentResponse[].class).getBody();
+        CommentResponse[] responses = restTemplate.getForEntity(urlAll, CommentResponse[].class).getBody();
         return Arrays.asList(responses);
     }
 }
--- core/src/main/java/terabu/service/UserService.java
+++ core/src/main/java/terabu/service/UserService.java
@@ -1,6 +1,8 @@
 package terabu.service;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +14,31 @@ import terabu.dto.users.UserDTO;
 @RequiredArgsConstructor
 public class UserService {
     private final RestTemplate restTemplate;
-    final String URL = "http://localhost:8081/users";
+
+    @Value("${user.url}")
+    private String url;
+    private String urlByLogin;
+    private String urlByEmail;
+    private String urlById;
+
+    @PostConstruct
+    public void init() {
+        urlByLogin = url + "/findByLogin?login=";
+        urlByEmail = url + "/findByUser?email=";
+        urlById = url + "/findById?id=";
+    }
+
+
     public UserDTO findUserByLogin(@RequestParam String login){
-        return restTemplate.getForEntity("http://localhost:8081/user/findByLogin?login=" + login, UserDTO.class).getBody();
+        return restTemplate.getForEntity(urlByLogin + login, UserDTO.class).getBody();
     }
 
     public UserDTO findUserByEmail(@RequestParam String email){
-        return restTemplate.getForEntity("http://localhost:8081/user/findByUser?email=" + email, UserDTO.class).getBody();
+        return restTemplate.getForEntity(urlByEmail + email, UserDTO.class).getBody();
     }
 
     public UserDTO findUserById(@RequestParam Long id){
-        return restTemplate.getForEntity("http://localhost:8081/user/findById?id=" + id, UserDTO.class).getBody();
+        return restTemplate.getForEntity(urlById + id, UserDTO.class).getBody();
     }
 
 }
