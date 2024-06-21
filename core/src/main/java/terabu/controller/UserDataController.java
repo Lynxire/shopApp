ed6e87b6172711d5f4de2905ebe62f8fd6e278f7
Lynxire <ya.staravoitav@gmail.com>
2024-06-22 02:25:03 +0300

    fix update User Data

--- core/src/main/java/terabu/controller/UserDataController.java
+++ core/src/main/java/terabu/controller/UserDataController.java
@@ -3,8 +3,10 @@ package terabu.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +23,18 @@ import terabu.dto.data.UserDataResponse;
 public class UserDataController {
     private final RestTemplate restTemplate;
 
+    @Value("${user_data.url}")
+    private String url;;
+    private String urlUpdate;
+    @PostConstruct
+    public void init() {
+        urlUpdate = url + "/update";
+    }
+
     @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("/update")
     @Operation(summary = "Обновление личной информации пользователя", description = "Данный метод для обновления личной информации и данных для входа")
     public ResponseEntity<UserDataResponse> updateData(@RequestBody @Valid UserDataRequest userDataRequest){
-        return restTemplate.postForEntity("http://localhost:8081/data/update", userDataRequest, UserDataResponse.class);
+        return restTemplate.postForEntity(urlUpdate, userDataRequest, UserDataResponse.class);
     }
 }
